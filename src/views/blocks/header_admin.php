7a334e0f64a0b3d4fd28daf320f92192326c7edc
Svetlana Samsonova <s.yu.samsonova@gmail.com>
2023-06-07 16:53:43 +0300

    Final commit

--- src/views/blocks/header_admin.php
+++ src/views/blocks/header_admin.php
@@ -1,5 +1,5 @@
 <header class="d-flex flex-wrap py-3">
-    <h4 id="logo">ПРИГОРОД ЛЕСНОЕ</h4>
+    <h4 id="logo">СЕРЕБРЯНАЯ ПОДКОВА</h4>
     <ul id="items" class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
         <li><a href="?page=news_read" id="header_link" class="nav-link px-2">Новости</a></li>
         <li><a href="?page=users" id="header_link" class="nav-link px-2">Пользователи</a></li>
--- src/views/blocks/header_guard.php
+++ src/views/blocks/header_guard.php
@@ -1,5 +1,5 @@
 <header class="d-flex flex-wrap py-3">
-    <h4 id="logo">ПРИГОРОД ЛЕСНОЕ</h4>
+    <h4 id="logo">СЕРЕБРЯНАЯ ПОДКОВА</h4>
     <ul id="items" class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
         <li><a href="?page=news_read" id="header_link" class="nav-link px-2">Новости</a></li>
         <li><a href="?page=my_applications" id="header_link" class="nav-link px-2">Заявления</a></li>
